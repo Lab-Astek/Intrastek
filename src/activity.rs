09f0ecc8677f97eac84553bba0265396ff83f216
MartinFillon <martin.fillon@hotmail.com>
2024-07-01 10:25:21 +0200

    feat: start activity trait

---
+++ src/activity.rs
@@ -0,0 +1,13 @@
+use chrono::{DateTime, Local};
+
+pub enum Activities {
+    FollowUp,
+    Bootstrap,
+    Review,
+    Keynote,
+    Surveillance
+}
+
+pub trait Activity {
+    fn get_time(&self) -> DateTime<Local>;
+}
