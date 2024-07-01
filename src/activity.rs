838a59d3713a6dce00dcad6467737797bc5f92ae
MartinFillon <martin.fillon@hotmail.com>
2024-07-01 10:42:02 +0200

    feat: add serialization and deserialization

--- src/activity.rs
+++ src/activity.rs
@@ -1,5 +1,7 @@
 use chrono::{DateTime, Local};
+use serde::{Deserialize, Serialize};
 
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub enum Activities {
     FollowUp,
     Bootstrap,
@@ -8,8 +10,19 @@ pub enum Activities {
     Surveillance,
 }
 
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Activity {
     time: DateTime<Local>,
     activity: Activities,
     location: String,
+}
+
+impl Activity {
+    pub fn new(time: DateTime<Local>, activity: Activities, location: &str) -> Self {
+        Activity {
+            time,
+            activity,
+            location: location.to_string(),
+        }
+    }
 }
