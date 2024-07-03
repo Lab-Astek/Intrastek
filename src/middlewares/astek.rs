81fd428915fd8518d55ec6bfde8e4d70be6629f7
MartinFillon <martin.fillon@hotmail.com>
2024-07-03 14:39:03 +0200

    refactor: create state middlewares

--- src/middlewares/astek.rs
+++ src/middlewares/astek.rs
@@ -6,49 +6,33 @@ use uuid::Uuid;
 
 use crate::{astek::Astek, helpers::IntrastekErrors, state::IntrastekState};
 
+use super::get_state;
+
 pub fn get_astek_and_then<T>(
     id: Uuid,
     state: &State<Mutex<IntrastekState>>,
     callback: impl Fn(&Arc<RwLock<Astek>>) -> Result<T, IntrastekErrors>,
 ) -> Result<T, IntrastekErrors> {
-    match state.lock() {
-        Ok(mutex) => {
-            if let Some(astek) = mutex
-                .asteks
-                .iter()
-                .find(|a| a.as_ref().read().is_ok_and(|x| x.id == id))
-            {
-                callback(astek)
-            } else {
-                Err(IntrastekErrors::NotFound(id))
-            }
+    get_state(state, |mutex| {
+        if let Some(astek) = mutex
+            .asteks
+            .iter()
+            .find(|a| a.as_ref().read().is_ok_and(|x| x.id == id))
+        {
+            callback(astek)
+        } else {
+            Err(IntrastekErrors::NotFound(id))
         }
-        Err(e) => {
-            error!("{:?}", e);
-            Err(IntrastekErrors::InternalError)
-        }
-    }
+    })
 }
 
 pub fn get_astek(id: Uuid, state: &State<Mutex<IntrastekState>>) -> Result<Astek, IntrastekErrors> {
-    match state.lock() {
-        Ok(mutex) => {
-            if let Some(astek) = mutex
-                .asteks
-                .iter()
-                .find(|a| a.as_ref().read().is_ok_and(|x| x.id == id))
-            {
-                match astek.as_ref().read() {
-                    Ok(astek) => Ok(astek.clone()),
-                    Err(_) => Err(IntrastekErrors::InternalError),
-                }
-            } else {
-                Err(IntrastekErrors::NotFound(id))
-            }
-        }
-        Err(e) => {
-            error!("{:?}", e);
+    get_astek_and_then(id, state, |astek| {
+        if let Ok(astek) = astek.as_ref().read() {
+            Ok(astek.clone())
+        } else {
+            error!("Failed to read astek");
             Err(IntrastekErrors::InternalError)
         }
-    }
+    })
 }
--- src/middlewares/mod.rs
+++ src/middlewares/mod.rs
@@ -1,1 +1,27 @@
+use std::sync::Mutex;
+
+use rocket::State;
+
+use crate::{helpers::IntrastekErrors, state::IntrastekState};
+
 pub mod astek;
+
+pub fn get_state<T>(
+    state: &State<Mutex<IntrastekState>>,
+    f: impl Fn(&IntrastekState) -> Result<T, IntrastekErrors>,
+) -> Result<T, IntrastekErrors> {
+    match state.lock() {
+        Ok(mutex) => f(&mutex),
+        Err(_) => Err(IntrastekErrors::InternalError),
+    }
+}
+
+pub fn get_state_mut<T>(
+    state: &State<Mutex<IntrastekState>>,
+    f: impl Fn(&mut IntrastekState) -> Result<T, IntrastekErrors>,
+) -> Result<T, IntrastekErrors> {
+    match state.lock() {
+        Ok(mut mutex) => f(&mut mutex),
+        Err(_) => Err(IntrastekErrors::InternalError),
+    }
+}
