034ac4019d812e38f2453bf6b4c6a603a3b65e46
MartinFillon <martin.fillon@hotmail.com>
2024-07-05 10:20:37 +0200

    feat: add radio buttons to select the activity type

--- frontend/src/app/activity/create/page.tsx
+++ frontend/src/app/activity/create/page.tsx
@@ -1,43 +1,57 @@
 "use client";
 
-import { FormEvent, useState } from 'react'
+import { FC, ChangeEvent, ReactElement, useState } from 'react'
+import FormControl from '@mui/material/FormControl';
+import { ActivitiyType } from '@/types/activity';
+import RadioWrapper from '@/components/inputs/radio'
+
+type ActivityPickerProp = {
+  onUpdate?: (arg0: ChangeEvent<HTMLInputElement>) => void
+}
+
+const ActivityPicker: FC<ActivityPickerProp> = ({onUpdate = undefined}): ReactElement => {
+  let activities = [
+    ActivitiyType.Permanence,
+    ActivitiyType.FollowUp,
+    ActivitiyType.Bootstrap,
+    ActivitiyType.Keynote,
+    ActivitiyType.Review,
+    ActivitiyType.Surveillance
+  ];
+
+  return (
+    <div className="activity-picker">
+      <FormControl>
+        <RadioWrapper defaultValue={activities[0]} name="activity" row onUpdate={onUpdate}>
+          {activities.map((s, idx) => s)}
+        </RadioWrapper>
+      </FormControl>
+    </div>
+  )
+}
 
 export default function Home() {
   let [location, setLocation] = useState<string>('');
   let [asteks, setAsteks] = useState<number>(0);
+  let [activitiyType, setActivityType] = useState<ActivitiyType>();
 
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+  async function onSubmit(event: ChangeEvent<HTMLFormElement>) {
     event.preventDefault()
 
     console.log(location)
     console.log(asteks)
   }
 
-  function handleChangeLocation(event: FormEvent<HTMLInputElement>) {
+  function handleChangeLocation(event: ChangeEvent<HTMLInputElement>) {
     setLocation(event.currentTarget.value)
   }
 
-  function handleChangeAsteks(event: FormEvent<HTMLInputElement>) {
+  function handleChangeAsteks(event: ChangeEvent<HTMLInputElement>) {
     setAsteks(parseInt(event.currentTarget.value))
   }
 
-  function ActivityPicker() {
-    return (
-      <div>
-        <label htmlFor="followup">FollowUp</label>
-        <input type="radio" id="followup" name="activity" value="FollowUp" />
-        <label htmlFor="bootstrap">Bootstrap</label>
-        <input type="radio" id="bootstrap" name="activity" value="Bootstrap" />
-        <label htmlFor="review">Review</label>
-        <input type="radio" id="review" name="activity" value="Review" />
-        <label htmlFor="keynote">Keynote</label>
-        <input type="radio" id="keynote" name="activity" value="Keynote" />
-        <label htmlFor="surveillance">Surveillance</label>
-        <input type="radio" id="surveillance" name="activity" value="Surveillance" />
-        <label htmlFor="permanence">Permanence</label>
-        <input type="radio" id="permanence" name="activity" value="Permanence" />
-      </div>
-    )
+  function onUpdateActivity(event: ChangeEvent<HTMLInputElement>) {
+    setActivityType(event.currentTarget.value as ActivitiyType)
   }
 
   return (
@@ -49,8 +63,12 @@ export default function Home() {
         <label htmlFor="asteks">Asteks</label><br></br>
         <input type="number" name="asteks" onChange={handleChangeAsteks} /><br></br>
 
-        <ActivityPicker />
+        <ActivityPicker onUpdate={onUpdateActivity}/>
         <button type="submit">Submit</button>
+
+        <br></br>
+        <br></br>
+        <h2>{activitiyType}, {location}, {asteks}</h2>
       </form>
     </div>
   )
