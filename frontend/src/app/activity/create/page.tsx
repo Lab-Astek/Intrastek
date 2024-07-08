06e01639fe5b8853804c987c008826cc42a4ac9e
MartinFillon <martin.fillon@hotmail.com>
2024-07-08 17:32:13 +0200

    feat(frontend): created a date picker for the activity creation

--- frontend/src/app/activity/create/page.tsx
+++ frontend/src/app/activity/create/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { ChangeEvent, ReactElement, useState } from 'react'
-import { ActivitiyType, Activity } from '@/types/activity';
+import { ActivitiyType } from '@/types/activity';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import { SelectChangeEvent } from '@mui/material/Select';
 import SelectWrapper from '@/components/inputs/select';
 import ButtonWrapper from '@/components/button';
 import { createActivity, getActivity } from '@/api/activity';
-import { UUID } from 'crypto';
 import { Astek } from '@/types/astek';
+import Stack from '@mui/material/Stack';
+import DatePicker from '@/components/inputs/datePicker';
 
 const activities = [
   ActivitiyType.Permanence,
@@ -31,15 +32,17 @@ const modules = [
 ];
 
 export default function Home() {
-  let [location, setLocation] = useState<string>('');
+  let [name, setName] = useState<string>('');
   let [asteks, setAsteks] = useState<number>(0);
   let [activitiyTypeIdx, setActivityTypeIdx] = useState<number>(0);
   let [moduleIdx, setModuleIdx] = useState<number>(0);
+  let [startDate, setStartDate] = useState<Date>();
+  let [endDate, setEndDate] = useState<Date>();
 
   let [result, setResult] = useState<Astek>();
 
-  function handleChangeLocation(event: ChangeEvent<HTMLInputElement>) {
-    setLocation(event.currentTarget.value)
+  function handleChangeName(event: ChangeEvent<HTMLInputElement>) {
+    setName(event.currentTarget.value)
   }
 
   function handleChangeAsteks(event: ChangeEvent<HTMLInputElement>) {
@@ -75,36 +78,41 @@ export default function Home() {
   }
 
   function handleSubmit() {
-    console.log(location, asteks, activities[activitiyTypeIdx], modules[moduleIdx]);
-    createActivity(location, asteks, activities[activitiyTypeIdx], modules[moduleIdx]).then((response) => {
-      getActivity(response.data).then((resp) => {
-        setResult(resp.data)
-      })
-    })
+    if (startDate === undefined || endDate === undefined)
+      return;
+    createActivity(name, asteks, activities[activitiyTypeIdx], modules[moduleIdx], { start: startDate, end: endDate })
   }
 
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <form noValidate autoComplete="off">
-        <Box
-          component="div"
-          sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
-          }}
-        >
-          <TextField label="Location" margin="dense" focused onChange={handleChangeLocation} />
-          <TextField defaultValue={0} type="number" label="Asteks" margin="dense" focused onChange={handleChangeAsteks} />
-        </Box>
-        <br></br>
-        <ActivityPicker />
-        <br></br>
-        <ModulePicker />
-        <br></br>
-        <ButtonWrapper onClick={handleSubmit}>
-          Create
-        </ButtonWrapper>
-        {JSON.stringify(result)}
+        <Stack spacing={3}>
+          <Box
+            component="div"
+            sx={{
+              '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+          >
+            <TextField label="Name" margin="dense" focused onChange={handleChangeName} />
+            <TextField defaultValue={0} type="number" label="Asteks" margin="dense" focused onChange={handleChangeAsteks} />
+          </Box>
+          <ActivityPicker />
+          <ModulePicker />
+          <DatePicker label="Start" onChange={(value, ctx) => {
+            if (value) {
+              setStartDate(value.toDate())
+            }
+          }} />
+          <DatePicker label="End" onChange={(value, ctx) => {
+            if (value) {
+              setEndDate(value.toDate())
+            }
+          }} />
+          <ButtonWrapper onClick={handleSubmit}>
+            Create
+          </ButtonWrapper>
+        </Stack>
       </form>
     </div>
   )
---
+++ frontend/src/components/inputs/datePicker.tsx
@@ -0,0 +1,16 @@
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { Dayjs } from "dayjs";
+import { DateTimeValidationError, PickerChangeHandlerContext } from "@mui/x-date-pickers/models";
+
+type DatePickerProps = {
+    label?: string;
+    onChange?: ((value: Dayjs | null, context: PickerChangeHandlerContext<DateTimeValidationError>) => void) | undefined
+}
+
+export default function DatePicker({ label = "", onChange }: DatePickerProps) {
+    return <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateTimePicker label={label} onChange={onChange} />
+    </LocalizationProvider>
+}
