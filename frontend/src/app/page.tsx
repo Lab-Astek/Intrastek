f67fbe72921c144d3b67ea15d5e2f35aa744f657
MartinFillon <martin.fillon@hotmail.com>
2024-07-16 15:04:49 +0200

    refactor: moved components around

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,118 +1,14 @@
 "use client";
-import { useState, useEffect } from "react";
-import { UUID, randomUUID } from "crypto";
-import { getAstek } from "../api/asteks";
-import { Astek } from "../types/astek";
-import ButtonWrapper from "@/components/button";
 import Page from "@/components/page";
-import SelectWrapper from "@/components/inputs/select";
-import { Box } from "@mui/material";
 
-import {
-  MsalProvider,
-  useMsal,
-  useAccount,
-  useIsAuthenticated,
-} from "@azure/msal-react";
-import { loginRequest } from "../authConfig";
+import { MsalProvider } from "@azure/msal-react";
 import {
   EventType,
   EventMessage,
   AuthenticationResult,
-  InteractionStatus,
 } from "@azure/msal-browser";
-import {
-  AuthenticatedTemplate,
-  UnauthenticatedTemplate,
-} from "@azure/msal-react";
-import { msalInstance } from "./msal/MsGraphApiCall";
-import { log_auth } from "../api/request";
-
-function LogoutButton() {
-  const { instance } = useMsal();
-
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-
-  function handleLogout() {
-    setAnchorEl(null);
-    instance.logoutPopup();
-  }
-
-  return <button onClick={handleLogout}>Logout</button>;
-}
-
-function LoginButton() {
-  const { instance } = useMsal();
-
-  function handleLogin() {
-    instance
-      .loginPopup(loginRequest)
-      .then((response) => {
-        sendTokenToBackend(response.accessToken);
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  }
-
-  async function sendTokenToBackend(token: string) {
-    try {
-      console.log("Sending token to backend...");
-      const response = await log_auth("POST", "asteks", token);
-      console.log("Success:", response.data);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
-
-  return <button onClick={handleLogin}>Login</button>;
-}
-
-function LoginAstekButton() {
-  const status = useMsal();
-  const isAuthenticated = useIsAuthenticated();
-
-  if (isAuthenticated) {
-    return <LogoutButton />;
-  } else if (
-    //@ts-ignore
-    status !== InteractionStatus.Startup &&
-    //@ts-ignore
-    status !== InteractionStatus.HandleRedirect
-  ) {
-    return <LoginButton />;
-  } else {
-    return null;
-  }
-}
-
-const AccountInfo = () => {
-  const { accounts } = useMsal();
-  const account = useAccount(accounts[0] || {});
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
-  useEffect(() => {
-    if (account && account.name) {
-      setName(account.name);
-      setEmail(account.username);
-    }
-  }, [account]);
-
-  if (name || email) {
-    return (
-      <>
-        <Box>
-          <h1>Active account: {name}</h1>
-          <h1>Email: {email}</h1>
-        </Box>
-      </>
-    );
-  } else {
-    return null;
-  }
-};
+import { msalInstance } from "@/components/msal/MsGraphApiCall";
+import { AccountInfo, LoginAstekButton } from "@/components/login";
 
 export default function Home() {
   msalInstance.initialize().then(() => {
